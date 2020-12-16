e01dc6fa1a13d74bdb7c7324e6d1562e7c9c3ae5
Cong Wang <wangimagine@gmail.com>
2020-12-15 18:32:31 -0600

    Add matrix support to utils regarding trigular matrix

--- pyrotein/utils.py
+++ pyrotein/utils.py
@@ -51,3 +51,66 @@ def read_file(file, numerical = False):
             if len(words) > 0: lines.append(words)
 
     return lines
+
+
+
+
+# [[[ Matrix operation ]]]
+
+def mat2tril(mat, keepdims = False, offset = 0):
+    ''' Convert a matrix into a lower triangular matrix.
+        mode:
+        - False: return one-dimensional array.
+        - True : return trigular matrix.
+    '''
+    # Convert full matrix to lower triangular matrix...
+    res = mat * np.tri(len(mat), len(mat), offset)
+
+    # Convert the lower triangular matrix into a one-dimensional array...
+    if not keepdims: res = res[np.tril_indices(len(mat), offset)]
+
+    return res
+
+
+
+
+def array2tril(ary, length, offset = 0):
+    ''' Convert a one-dimensional array into a lower triangular matrix.
+    '''
+    # Create an empty matrix with edge size of len...
+    res = np.zeros((length, length))
+
+    # Collect indices for members in lower triangular matrix with offset...
+    ver_i, hor_i = np.tril_indices(length, offset)
+
+    # Find the smaller length for valid assignment...
+    capacity        = len(ver_i)
+    area            = len(ary)
+    rightmost_index = np.min([area, capacity])
+
+    # Update empty matrix with values in the input array...
+    res[ver_i[:rightmost_index], hor_i[:rightmost_index]] = ary[:rightmost_index]
+
+    return res
+
+
+
+
+def fill_nan_with_mean(mat, axis = 0):
+    ''' Fill np.nan with mean value along `axis`.
+        Support two-dimensional matrix only.
+    '''
+    # Assert mat is 2d...
+    assert len(mat.shape) == 2, "fill_nan_with_mean ONLY supports 2D matrix."
+
+    # Obtain the axis mean...
+    axis_mean = np.nanmean(mat, axis = axis)
+
+    # Find the indices that has values of np.nan...
+    nan_i = np.where(np.isnan(mat))
+
+    # Replace np.nan with mean...
+    rep_axis = 1 - axis
+    mat[nan_i] = np.take(axis_mean, nan_i[rep_axis])
+
+    return None
