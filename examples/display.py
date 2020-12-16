d16f8490426fe3c5673bbf91ba1b0d3703a3f5b7
Cong Wang <wangimagine@gmail.com>
2020-12-15 23:48:01 -0600

    Add more Gnuplot visualization for SVD analysis in the example file

--- examples/display.py
+++ examples/display.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import pyrotein as pr
 import numpy as np
 import GnuplotPy3
 
@@ -11,6 +12,7 @@ def plot_dmat(
     lbl,                   # Labels used to mark on the diagonal
     lbl_font_size = 8,     # Fontsize for label
     palette       = "",    # Palette definition
+    intst_min     = "0",   # Min intensity value
     intst_max     = "*",   # Max intensity value
     smooth        = False, # Choices of styles: smooth vs pixelated
     upper         = 0.5,   # The value to facilitate the plot of lower triangle matrix
@@ -99,7 +101,7 @@ def plot_dmat(
         gp("set palette defined ( -0.001 'white', 0 'blue', 0.5 'light-grey', 1 'red' )")
     else:
         gp(palette)
-    gp(f"set cbrange [0:{intst_max}]")
+    gp(f"set cbrange [{intst_min}:{intst_max}]")
 
     for cmd in cmds_bottom:
         gp(cmd)
@@ -119,5 +121,84 @@ def plot_dmat(
             if j > k: gp(f"{k} {j} {dmat[j, k]}")
             else: gp(f"{k} {j} {upper}")
         gp(" ")
+    gp("e")
+    gp("exit")
+
+
+
+
+def plot_singular(s, eps = "singular.eps", log = False):
+    ''' Plot singular values.
+    '''
+    gp = GnuplotPy3.GnuplotPy3()
+    gp("set terminal postscript eps size 3.5, 2.62 enhanced color font 'Helvetica,14' linewidth 2")
+    gp(f"set output '{eps}'")
+    gp("unset key")
+    if log: gp("set log y")
+    gp("set xlabel 'Rank of singular values'")
+    gp("set ylabel 'Singular values'")
+    gp("plot '-' using 1:2 with linespoints linetype 2 linewidth 1 linecolor rgb 'black'")
+
+    for i, v in enumerate(s): gp(f"{i} {v}")
+    gp("e")
+    gp("exit")
+
+
+
+
+def plot_left_singular(u, rank, length_mat, frac = 0.1, binning = 4):
+    ''' Plot left singular value as a lower triangular distance matrix.
+    '''
+    # Convert `u` at position `rank` into a lower triangular matrix...
+    dmat = pr.utils.array2tril(u[:, rank], length_mat, offset = -1)
+
+    # Define a color palette...
+    pal = "set palette defined \
+           (-10.001 'white', -10 '#800000', -5 'red', -1 'white', 0 'seagreen', \
+              1 'white'  , 5 'blue', 10 'navy')"
+
+    # Filename to export...
+    eps = f"u{rank:02d}.eps"
+
+    # Export eps...
+    dmat_bin = pr.utils.bin_image(dmat, binning = binning)
+    dmat_bin_check = dmat_bin.copy()
+
+    # Find the full range...
+    bound = np.max(np.abs([np.min(dmat_bin), np.max(dmat_bin)]))
+    intst_min = -bound * frac
+    intst_max =  bound * frac
+
+
+    plot_dmat(dmat_bin, 
+              eps, 
+              lbl = [""] * len(dmat_bin), 
+              intst_min = intst_min,
+              intst_max = intst_max,
+              palette = pal, 
+              upper  = intst_min - 1,
+              smooth = True)
+
+
+
+
+def plot_coeff(c, rank1, rank2, labels, offset = '2.0,0.0', rot = 0):
+    ''' Scatter plot of examples from 2 dimensions specified by rank1 and rank2.
+    '''
+    gp = GnuplotPy3.GnuplotPy3()
+    gp("set terminal postscript eps size 3.5, 2.62 enhanced color font 'Helvetica,14' linewidth 2")
+    gp(f"set output 'coeff_{rank1:02d}vs{rank2:02d}.eps'")
+    gp("unset key")
+    gp(f"set xlabel 'c_{{{rank1:02d}}}'")
+    gp(f"set ylabel 'c_{{{rank2:02d}}}'")
+
+    gp( "plot '-' using 1:2   with point linewidth 1.5 pointtype 6 pointsize 0.5 linecolor rgb '#003f5c', \\")
+    gp(f"     '-' using 1:2:3 with labels rotate by {rot} offset char {offset} font ',8', \\")
+    gp("")
+    for i in range(len(c[0])): 
+        gp(f"{c[rank1, i]} {c[rank2,i]}")  
+    gp("e")
+    for i in range(len(c[0])): 
+        gp(f"{c[rank1, i]} {c[rank2,i]} {labels[i]}")  
     gp("e")
     gp("exit")
